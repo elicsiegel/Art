7b796b6c85d40636a60788d4a506312e65e33201
Eli <elicsiegel@gmail.com>
2017-10-07 20:12:14 -0400

    Link styles for search bar

--- frontend/reducers/search_reducer.js
+++ frontend/reducers/search_reducer.js
@@ -6,7 +6,7 @@ const SearchesReducer = (state = defaultState, action) => {
   switch(action.type){
 
     case CLEAR_SEARCH_RESULTS:
-      return Object.assign( {}, state, defaultState );
+      return Object.assign( {}, defaultState );
 
     case RECEIVE_SEARCH_RESULTS:
       
