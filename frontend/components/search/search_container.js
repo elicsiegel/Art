3fdcb165fb20af0d801773ebcc716bcd9d4fee78
Eli <elicsiegel@gmail.com>
2017-11-27 15:52:10 -0500

    Clean up search container

--- frontend/components/search/search_container.js
+++ frontend/components/search/search_container.js
@@ -1,23 +1,17 @@
 import { connect } from 'react-redux';
 import Search from './search';
 import { selectArtistsFromSearchResults } from '../../reducers/selectors';
-// import { activateDropdown, deactivateDropdown } from '../../actions/dropdown_actions';
 import { sendSearchQuery, clearSearchResults } from '../../actions/search_actions';
 
 const mapStateToProps = state => {
   return {
     artistResults: selectArtistsFromSearchResults(state.search),
-    // userResults: relevantUserSearchResults(state),
-    // searchBarVisible: state.dropdowns["searchBar"],
-    // searchResultsVisible: state.dropdowns["searchResults"],
   };
 }
 
 const mapDispatchToProps = dispatch => ({
   clearSearchResults: () => dispatch(clearSearchResults()),
   sendSearchQuery: searchQuery => dispatch(sendSearchQuery(searchQuery)),
-  // showSearchResults: () => dispatch(activateDropdown('searchResults')),
-  // showSearchBar: () => dispatch(activateDropdown('searchBar')),
 });
 
 export default connect(
