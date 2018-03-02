569d4e29e44b3e184bd420d31b1a5af43f4d5a72
Eli <elicsiegel@gmail.com>
2018-03-02 10:17:05 -0500

    Update token

--- frontend/util/artists_api_util.js
+++ frontend/util/artists_api_util.js
@@ -1,4 +1,4 @@
-export const xappToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsImV4cCI6MTUxOTY1NjE3NywiaWF0IjoxNTE5MDUxMzc3LCJhdWQiOiI1OWRmYWI4M2EwOWE2NzI1NDk0YzNiZjAiLCJpc3MiOiJHcmF2aXR5IiwianRpIjoiNWE4YWUyNzE4YjNiODEzNzhjMWJjOTc1In0.sIln-9-zLtfkuTjrrZON4XBouxEdWamGSK5RdmzUy9k";
+export const xappToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsImV4cCI6MTUyMDYwODU0OSwiaWF0IjoxNTIwMDAzNzQ5LCJhdWQiOiI1OWRmYWI4M2EwOWE2NzI1NDk0YzNiZjAiLCJpc3MiOiJHcmF2aXR5IiwianRpIjoiNWE5OTZhYTVjZDUzMGU1YWIwMjc4MWJhIn0.QYC1F7Kc0D6A-wGxjgoqv2o4fs7uFy4qOI2ufJg1PTc";
 
 export const fetchArtist = (slug_name) => {
   return  $.ajax({
