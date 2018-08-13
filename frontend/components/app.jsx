0e8d273f41bee842863781201233fd7fa55790f9
Eli <elicsiegel@gmail.com>
2018-08-13 19:07:42 -0400

    Add fixed nav

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,28 +2,11 @@ import React from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 import SearchContainer from './search/search_container';
 import ArtistDetailContainer from './artists/artist_detail_container';
+import Nav from './nav';
 
 const App = (props) => (
   <div>
-    <div className="title-div">
-      <div className="author-info">
-        <a href="http://www.elicsiegel.com">
-          <h4>Eli Siegel</h4> 
-        </a>
-        <div className="links-div"> 
-          <a href="https://www.linkedin.com/in/eli-siegel-a26a3b73/">
-            <img src="assets/linkedin.png" alt="LinkedIn"/>
-          </a>
-          <a href="https://github.com/elicsiegel">
-            <img src="assets/github.svg" alt="Github"/>
-          </a>
-        </div>
-      </div>
-      <h1>Art Map</h1>
-    </div>
-    <div className="search-outer-div">
-      <SearchContainer/>
-    </div>
+    <Nav/>
     <div>
       <Route path="/artists/:artistName" exact component={ArtistDetailContainer} />
     </div>
---
+++ frontend/components/nav.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import SearchContainer from './search/search_container';
+
+const Nav = (props) => (
+    <div className="title-div">
+      <h1>Art Map</h1>
+      <div className="search-outer-div">
+        <SearchContainer/>
+      </div>
+      <div className="author-info">
+        <a href="http://www.elicsiegel.com">
+          <h4>Eli Siegel</h4> 
+        </a>
+        <div className="links-div"> 
+          <a href="https://www.linkedin.com/in/eli-siegel-a26a3b73/">
+            <img src="assets/linkedin.png" alt="LinkedIn"/>
+          </a>
+          <a href="https://github.com/elicsiegel">
+            <img src="assets/github.svg" alt="Github"/>
+          </a>
+        </div>
+      </div>
+    </div>
+);
+
+export default Nav;
