01c05ab65bc318d0b12d198cee2d20530f0d16a8
Eli <elicsiegel@gmail.com>
2018-09-23 12:36:05 -0400

    Update token

--- bundle.js
+++ bundle.js
@@ -1780,7 +1780,7 @@ var createTransitionManager = function createTransitionManager() {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var xappToken = exports.xappToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsImV4cCI6MTUzNzMxMTU2MiwiaWF0IjoxNTM2NzA2NzYyLCJhdWQiOiI1OWQ1YTkxZmM5ZGMyNDE5MGIwNjkxMzYiLCJpc3MiOiJHcmF2aXR5IiwianRpIjoiNWI5ODQ4Y2EzZDNkZDQzNDVlYzJjMmU0In0.aJUjzv82dZDUX-mwmqENyan_TlQ1EpvSlItVipqv1U8";
+var xappToken = exports.xappToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsImV4cCI6MTUzODMyNTI3MiwiaWF0IjoxNTM3NzIwNDcyLCJhdWQiOiI1OWQ1YTkxZmM5ZGMyNDE5MGIwNjkxMzYiLCJpc3MiOiJHcmF2aXR5IiwianRpIjoiNWJhN2MwOThmMTkyMGU0NTdjZmJjN2UxIn0.CT_nBFBtfRLAp74qI3AcCGaX0D4DEqgVVs4VWfSIsD4";
 
 var fetchArtist = exports.fetchArtist = function fetchArtist(slug_name) {
   return $.ajax({
