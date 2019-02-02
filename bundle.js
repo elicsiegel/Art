3e8adba41224850f507547a685df0ad5c85c0e51
Eli <elicsiegel@gmail.com>
2019-02-02 15:59:24 -0500

    Update token

--- bundle.js
+++ bundle.js
@@ -1780,7 +1780,8 @@ var createTransitionManager = function createTransitionManager() {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var xappToken = exports.xappToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsImV4cCI6MTU0MTE3NDI0NywiaWF0IjoxNTQwNTY5NDQ3LCJhdWQiOiI1OWQ1YTkxZmM5ZGMyNDE5MGIwNjkxMzYiLCJpc3MiOiJHcmF2aXR5IiwianRpIjoiNWJkMzM5Njc0ZjMyZGE3ZmYwYzY3MGMyIn0.Xf7pVXHomsSdGzcYR2Kv2L3RKxqErjnycUjpnIbqaQA";
+var xappToken = exports.xappToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsImV4cCI6MTU0OTc0NTkxMSwiaWF0IjoxNTQ5MTQxMTExLCJhdWQiOiI1OWQ1YTkxZmM5ZGMyNDE5MGIwNjkxMzYiLCJpc3MiOiJHcmF2aXR5IiwianRpIjoiNWM1NjA0Nzc3MjdlZDg2MGNiNTQ5NDdmIn0.B9SDSO1jhX2eUlZVOvEiUJcPU4CAHTHt62dGBVfmNtc";
+// curl -v -X POST "https://api.artsy.net/api/tokens/xapp_token?client_id=2d13c6496b8d0f11c69d&client_secret=fad661bf31512a3812a24ccb59488f5a"
 
 var fetchArtist = exports.fetchArtist = function fetchArtist(slug_name) {
   return $.ajax({
