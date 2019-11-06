9395b6cbbe7468daf84d333a9ab9d23f97c2cd0f
Eli <elicsiegel@gmail.com>
2019-11-06 11:39:42 -0500

    update token

--- bundle.js
+++ bundle.js
@@ -1780,7 +1780,7 @@ var createTransitionManager = function createTransitionManager() {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var xappToken = exports.xappToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI1OWQ1YTkxZmM5ZGMyNDE5MGIwNjkxMzYiLCJleHAiOjE1NzE3NzQ5NjYsImlhdCI6MTU3MTE3MDE2NiwiYXVkIjoiNTlkNWE5MWZjOWRjMjQxOTBiMDY5MTM2IiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjVkYTYyNzc2N2ZhMmY5MDAxMjkzZDE4NSJ9.IDbtw82FGQsl7pRmyUNz23mspsE5Sy4jNHG6pZ6rhlc";
+var xappToken = exports.xappToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI1OWQ1YTkxZmM5ZGMyNDE5MGIwNjkxMzYiLCJleHAiOjE1NzM2NjMxMjAsImlhdCI6MTU3MzA1ODMyMCwiYXVkIjoiNTlkNWE5MWZjOWRjMjQxOTBiMDY5MTM2IiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjVkYzJmNzEwNmYzYzQyMDAxMmYwM2QyNiJ9.fwQ0NzatrknVT3B3QJz7gR77wAL-1opIY3ynYl0tKe4";
 // curl -v -X POST "https://api.artsy.net/api/tokens/xapp_token?client_id=2d13c6496b8d0f11c69d&client_secret=fad661bf31512a3812a24ccb59488f5a"
 
 var fetchArtist = exports.fetchArtist = function fetchArtist(slug_name) {
